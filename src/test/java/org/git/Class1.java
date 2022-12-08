48e0350e52094d5955b9c0dea5804063d467701e
Dineshashu <dineshshashsudin025@gmail.com>
2022-12-08 13:32:54 +0530

    vanakam da mapla

---
+++ src/test/java/org/git/Class1.java
@@ -0,0 +1,18 @@
+package org.git;
+
+public class Class1 {
+	
+	 private void method1() {
+		 System.out.println();
+	}
+	 
+	 private void method2() {
+		 System.out.println();
+		}
+	 
+	 private void method3() {
+		 System.out.println();
+		}
+
+
+}
